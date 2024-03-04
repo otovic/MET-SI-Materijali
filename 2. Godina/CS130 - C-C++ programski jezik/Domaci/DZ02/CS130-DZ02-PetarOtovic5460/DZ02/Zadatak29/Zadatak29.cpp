a936e27823e63a28f9198323a5577f584db9d606
otovic <otovicpetar1998@gmail.com>
2024-03-04 10:13:35 +0100

    update

---
+++ 2. Godina/CS130 - C-C++ programski jezik/Domaci/DZ02/CS130-DZ02-PetarOtovic5460/DZ02/Zadatak29/Zadatak29.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+//Napisati logičku funkciju koja određuje da li je zbir cifara nekog celog broja paran.Testirati funkciju u glavnom programu.
+int main()
+{
+    int num = 0;
+    char numS[10];
+    int sum = 0;
+
+    printf("Unesite broj: ");
+    scanf_s("%d", &num);
+    sprintf_s(numS, "%d", num);
+
+    for (int i = 0; i < strlen(numS); i++) {
+        sum = sum + (numS[i] - '0');
+    }
+
+    printf("\nSuma cifara je: %d", sum);
+}
